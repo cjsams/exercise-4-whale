68fa85b42a9491116da7d0c5b8d21f460db1576c
cjsams <csams@mail.bradley.edu>
2015-10-19 11:58:31 -0500

    ) del

--- whale.js
+++ whale.js
@@ -1,10 +1,11 @@
 jQuery(document).ready(function($){
   var num = parseInt(location.hash.substr(1));
   console.log(num);
+
   $.get('/whale/txt/' + num + '.txt', function(txt)){
     $('a').html(txt);
     $('a').attr('href', '#' + (num + 1));
-  });
+  };
 
   if (isNaN(num)) {
     num = 0;
