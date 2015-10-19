12142048dabf651bd81ec0fc4e52014dc428fcda
cjsams <csams@mail.bradley.edu>
2015-10-19 11:50:42 -0500

    $.get

--- whale.js
+++ whale.js
@@ -1,9 +1,7 @@
-jQuery(document).ready(function($)
-{
+jQuery(document).ready(function($){
   var num = parseInt(location.hash.substr(1));
   console.log(num);
-  $.get('/whale/txt/' + num + '.txt', function(txt))
-  {
+  $.get('/whale/txt/' + num + '.txt', function(txt)){
     console.log(txt);
   });
 
