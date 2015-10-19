96c609c3219d32113dc9686f749c65dd2a612c2d
cjsams <csams@mail.bradley.edu>
2015-10-19 11:44:35 -0500

    i think i did this wright

--- whale.js
+++ whale.js
@@ -2,8 +2,12 @@ jQuery(document).ready(function($)
 {
   var num = parseInt(location.hash.substr(1));
   console.log(num);
+  $.get('/whale/txt/' + num + '.txt', function(txt))
+  {
+    console.log(txt);
+  });
 
   if (isNaN(num)) {
     num = 0;
-}
+  }
 });
