1a7fe55c0afc57297a9e0151db62a06e6df7a003
cjsams <csams@mail.bradley.edu>
2015-10-19 12:05:35 -0500

    commit

--- whale.js
+++ whale.js
@@ -1,11 +1,14 @@
 jQuery(document).ready(function($){
   var num = parseInt(location.hash.substr(1));
+  if (isNaN(num)) {
+    num = 0;
+  }
   console.log(num);
 
   $.get('/whale/txt/' + num + '.txt', function(txt)){
     $('a').html(txt);
     $('a').attr('href', '#' + (num + 1));
-  };
+  });
 
   if (isNaN(num)) {
     num = 0;
